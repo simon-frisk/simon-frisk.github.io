000249029e9b17e58d9a70c6e385931b65f94d24
Simon Frisk <simon@simonfrisk.com>
2026-03-12 12:44:29 -0500

    Add papers

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
         <div className={styles.header_text}>
           <div>
             <h1>Simon Frisk</h1>
-            <p>I'm a Ph.D. student at <a href="https://www.wisc.edu">University of Wisconsin-Madison</a>. I am most fortunate to be advised by Prof. <a href="https://pages.cs.wisc.edu/~paris/">Paraschos Koutris</a> in the <a href="https://database.cs.wisc.edu">database group</a>. I am interested in query optimization, distributed query processing, recursive query processing and more.</p>
+            <p>I'm a Ph.D. student at <a href="https://www.wisc.edu">University of Wisconsin-Madison</a>. I am most fortunate to be advised by Prof. <a href="https://pages.cs.wisc.edu/~paris/">Paraschos Koutris</a> in the <a href="https://database.cs.wisc.edu">database group</a>. I am broadly interested in query optimization.</p>
           </div>
           <div style={{
             display: "flex",
@@ -26,15 +26,27 @@ export default function Home() {
             marginTop: 10
           }}>
             <p>simon.frisk [at] wisc.edu</p>
+            <a href="https://www.linkedin.com/in/simon-frisk-a773a31b5/">LinkedIn</a>
             <a href="https://scholar.google.com/citations?user=1eRxj_oAAAAJ&hl=en">Google Scholar</a>
           </div>
         </div>
       </div>
-      <h2>Publications</h2>
+      <h2>Research</h2>
+      <Publication
+        name="𝜅-Join: Combining Vertex Covers for Parallel Joins"
+        authorlist="Simon Frisk, Austen Fan, Paraschos Koutris"
+        arxiv="https://arxiv.org/abs/2603.10177"
+      />
+      <Publication
+        name="One Join Order Does Not Fit All: Reducing Intermediate Results with Per-Split Query Plans"
+        authorlist="Yujun He, Hangdong Zhao, Simon Frisk, Yifei Yang, Kevin Kristensen, Paraschos Koutris, Xiangyao Yu"
+        arxiv="https://arxiv.org/abs/2510.25684"
+      />
       <Publication
         name="FlowLog: Efficient and Extensible Datalog via Incrementality"
         authorlist="Hangdong Zhao, Zhenghong Yu, Srinag Rao, Simon Frisk, Zhiwei Fan, Paraschos Koutris"
         venue="VLDB 2026"
+        paper="https://www.vldb.org/pvldb/vol19/p361-zhao.pdf"
         arxiv="https://arxiv.org/abs/2511.00865"
       />
       <Publication
@@ -47,5 +59,3 @@ export default function Home() {
     </div>
   );
 }
-// <a href="https://www.linkedin.com/in/simon-frisk-a773a31b5/">LinkedIn</a>
-// <a href="https://scholar.google.com/citations?user=1eRxj_oAAAAJ&hl=en">Google Scholar</a>
